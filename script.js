dd9882afa6791a089af055416ec915d6aa11904b
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-08-02 18:41:32 +0100

    Random colours generate and winning colour appears in h1

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-var colours = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
-
+var colours = generateRandomColours(6);
 var squares = document.querySelectorAll(".square");
-
 var pickedColour = pickColour();
-
 var colourDisplay = document.getElementById("colourDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
 
 colourDisplay.textContent = pickedColour;
 
@@ -25,14 +16,18 @@ for (var i = 0; i < squares.length; i++) {
     // grab colour of picked square
     // "this" refers to item that was clicked on
     var clickedColour = this.style.backgroundColor;
+
     // compare colour to picked colour
+    // CORRECT GUESS
     if (clickedColour === pickedColour) {
       // changes message above colours
       messageDisplay.textContent = "Correct!";
-      // if guessed right all changeColurs is
+      // if guessed right all changeColours is
       // passed the correct colour
       changeColours(clickedColour);
+      h1.style.backgroundColor = clickedColour;
     } else {
+      // INCORRECT GUESS
       this.style.backgroundColor = "#232323";
       // changes message above colours
       messageDisplay.textContent = "Try Again";
@@ -52,4 +47,32 @@ function changeColours(colour) {
 function pickColour() {
   var random = Math.floor(Math.random() * colours.length);
   return colours[random];
+}
+
+// Function which generates num random colours
+function generateRandomColours(num) {
+  //make an array
+  var arr = [];
+  // repeat num times
+  for (var i = 0; i < num; i++) {
+    // get random colour and push into array
+    arr.push(randomColour());
+  }
+  // return that array
+  return arr;
+}
+
+// The function which generates random colour
+function randomColour() {
+  // pick a "red" from 0 - 255
+  var r = Math.floor(Math.random() * 256); //256 as we are rounding down to max of 255
+  // pick a "green" from 0 - 255
+  var g = Math.floor(Math.random() * 256); // Math.floor rounds the number
+  // pick a "blue" from 0 - 255
+  var b = Math.floor(Math.random() * 256); // Math.random actually starts between 0 and 1 hence times by 256
+
+  // rgb(r, g, b); returning string
+  // Make sure spacing is correct or bug occurs where
+  // matched string does not equal correct
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
